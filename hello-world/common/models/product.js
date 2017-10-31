6524b9fd9312270d8cc5eab849cc8caef05c7077
dchau5 <dchau5@csc.com>
2017-10-31 16:18:39 +0700

    Update loopback part 1

--- hello-world/common/models/product.js
+++ hello-world/common/models/product.js
@@ -1,5 +1,42 @@
 'use strict';
 
 module.exports = function(Product) {
+    Product.getName = function(productId, cb) {
+        console.log(productId);
+        Product.findOne({where: {'ProductID' : productId } }, function (err, instance) {
+            console.log(instance);
 
+            var response = instance.ProductName;
+            cb(null, response);
+            console.log(response);
+        });
+    }
+    
+    Product.remoteMethod (
+        'getName',
+        {
+            http: {path: '/getName', verb: 'get'},
+            accepts: {arg: 'ProductID', type: 'number', required: true },
+            returns: {arg: 'ProductName', type: 'string'}
+        }
+    );
+
+    Product.getImage = function(productId, cb) {
+        console.log(productId);
+        Product.findOne({where: {'ProductID' : productId } }, function (err, instance) {
+            console.log(instance);
+
+            var response = instance.ProductImage;
+            cb(null, response);
+            console.log(response);
+        });
+    }
+    Product.remoteMethod (
+        'getImage',
+        {
+            http: {path: '/getImage', verb: 'get'},
+            accepts: {arg: 'ProductID', type: 'number', required: true },
+            returns: {arg: 'ProductImage', type: 'string'}
+        }
+    );
 };
